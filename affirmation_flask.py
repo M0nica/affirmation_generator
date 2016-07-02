a368aa7e6cdde78f67b0eeb5c392f26e81a9a556
Monica Powell <mmp2181@barnard.edu>
2016-07-02 18:04:08 -0400

    now only lists links for unique authors and list all quotes associated with an author

--- affirmation_flask.py
+++ affirmation_flask.py
@@ -5,20 +5,23 @@
 
 from flask import Flask, request, render_template
 import pg8000
+import os
 
 app = Flask(__name__)
 conn = pg8000.connect(database="affirmations", user="Monica")
 
-@app.route("/author")
+
+@app.route("/")
 def author_info():
-    ainfo = None
+    # ainfo = None
+    ainfo = list()
     allinfo = list()
     #default value for lookup_name is None
-    cname = request.args.get('lookup_name', None)
+    cname = request.args.get('author_name', None)
 
     cursor = conn.cursor()
     cursor.execute(
-    "SELECT author FROM quotes"
+    "SELECT DISTINCT author FROM quotes ORDER BY author ASC"
     )
     all_responses = cursor.fetchall()
     print("all_responses", all_responses)
@@ -32,17 +35,20 @@ def author_info():
         cursor.execute(
         "SELECT author, words FROM quotes WHERE author = %s", [cname]
         )
-        response = cursor.fetchone()
-        print(response)
+        responses = cursor.fetchall()
+        #print(response)
 
-        if response:
-            ainfo = {'name': response[0], 'words': response[1]}
+        for response in responses:
+            # ainfo = {'name': response[0], 'words': response[1]}
+            a_dict = {'name':response[0], 'words':response[1]}
+            ainfo.append(a_dict)
 
         print("AINFO IS", ainfo)
     print("info for all authors is", allinfo)
     #perform a database search
     # format the rresults as text (html)
     # return that text
+
     return render_template("author_lookup.html", author=ainfo, authors=allinfo)
 
 app.run(debug=True)

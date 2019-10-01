00bdc311000314e5967ca72fa0af99b424ae82b2
Riba Ishtiaq <areeba.aic@gmail.com>
2019-10-01 19:19:35 +0500

    Added Contribute button

--- src/AffirmationComponent.js
+++ src/AffirmationComponent.js
@@ -49,6 +49,9 @@ class AffirmationComponent extends React.Component {
           <button onClick={this.handleClick}>Next Affirmation</button>
           <TweetButton affirmation={affirmation.affirmation} author={affirmation.author} />
         </div>
+        <br />
+        <p>Want to add a new affirmation?</p>
+        <button><a href="https://github.com/M0nica/affirmation_generator" style="color: black; text-decoration: none;">Contribute</a></button>
       </div>
     );
   }

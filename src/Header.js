72af36d25ef8595c4157fe6699837aab0223e663
Mikalai Sokarau <mikalaisokarau@gmail.com>
2019-10-06 18:24:06 +0200

    refactor header, add header styles

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import lady_on_phone from "./lady_on_phone.svg";
-import "./App.css";
+import "./Header.css";
 
 class Header extends React.Component {
   render() {
     return (
-      <div className="Header">
+      <header>
         <h1>Affirmation Generator</h1>
         <img src={lady_on_phone} className="App-logo" alt="logo" />
-      </div>
+      </header>
     );
   }
 }
